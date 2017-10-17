0ec966c7f0f1baee930319968ee460cc86198f6b
Siravut <siratham@gmail.com>
2017-10-17 05:02:29 +0000

    Add pagination

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,8 @@
 class ChefsController < ApplicationController
   
   def index
-    @chefs = Chef.all
+    #@chefs = Chef.all
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -20,6 +21,7 @@ class ChefsController < ApplicationController
   
   def show
     @chef = Chef.find(params[:id])
+    @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
   end
   
   def edit
